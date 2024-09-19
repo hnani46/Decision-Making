7dc38abde0683db55a3eb258d77d810afd8fc038
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:06:18 +0530

    Create  CSE department.py

---
+++ CSE department.py
@@ -0,0 +1,11 @@
+...
+There are 3 labs in the CSE department are L1, L2, and L3 with a seating capacity of x, y, and z. A single lab needs to be allocated to a class of 'n' students. How many of the 3 labs can accommodate 'n' students?
+...
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+25
+Sample Output:
+2 
