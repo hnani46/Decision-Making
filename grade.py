b9cfb805e6eece17c9488253966c05084761f734
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 13:56:52 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,24 @@
+...
+The newly appointed Vice-Chancellor of Anna University wanted to create an automated grading system for the students to check their grade. When a student enters a mark, the grading system displays the corresponding grade.
+Write a program to solve the given problem.
+  Marks scored  
+  Grade 
+100
+S
+90 - 99
+A
+80 - 89
+B
+70 - 79
+C
+60 - 69
+D
+50 - 59
+E
+<50
+F
+
+...
+Sample Input:
+78
+Sample Output:C
