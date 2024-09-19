e2fc72a00a3fc7f794ffd42b309bf07e8eb5e213
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:01:56 +0530

    Create encoded.py

---
+++ encoded.py
@@ -0,0 +1,8 @@
+...
+Ask a user for their birth year encoded as two digits (like "62") and for the current year, also encoded as two digits (like "99"). Write a program to find the users current age in years
+...
+Sample Input:
+62
+00
+Sample Output:
+38
