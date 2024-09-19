2fb3e2937b843cf5b27f8758fdcccd17d62a1203
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:02:51 +0530

    Create labs.py

---
+++ labs.py
@@ -0,0 +1,9 @@
+....
+There are 3 labs in the CSE department(L1, L2, and L3) with a seating capacity of x, y, and z respectively. Find the lab which has minimal seating capacity.  
+...
+Sample Input:
+30
+40
+20
+Sample Output:
+L3
