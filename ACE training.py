39338938dad23144f7f6d1f3ad5528b3c95d4daa
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:04:28 +0530

    Create  ACE training.py

---
+++ ACE training.py
@@ -0,0 +1,10 @@
+....
+There are 3 labs in the CSE department are L1, L2, and L3 with a seating capacity of x, y, and z respectively. One of the 3 labs has been allocated for ACE training. Out of the available labs, find the lab which has minimal seating capacity.
+....
+Sample Input:
+30
+40
+20
+L3
+Sample Output:
+L1
