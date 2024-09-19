6926103a825afd0826f2bdb27854e99b75217830
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:07:46 +0530

    Create  CSE department2.py

---
+++ CSE department2.py
@@ -0,0 +1,10 @@
+....
+ There are 3 labs in the CSE department. The labs are L1, L2, and L3 with a seating capacity of x, y, and z respectively. A single lab needs to be allocated to a class of 'n' students. The labs need to be utilized to the maximum i.e the number of systems used should not be minimal. Which lab needs to be allocated to this class?
+...
+Sample Input:
+30
+40
+20
+25
+Sample Output:
+L1
