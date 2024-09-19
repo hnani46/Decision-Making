efbba75b1dd75b404c2ad6744140f7f4a2bcc75e
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:00:31 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,42 @@
+...
+Write a program to determine the fee amount to be collected from a student. 
+Refer the table below for fee details.
+  Student Type  
+  Student Type denoted as  
+  Fee Details  
+Merit Seat Day Scholar
+MSDS
+
+Tuition fee + Bus fee
+
+
+
+Merit Seat Hosteller
+MSH
+Tuition fee + Hostel fee
+
+Management Seat Day Scholar
+
+
+
+MGSDS
+
+150% of Tuition fee + Bus fee
+
+
+
+Management Seat Hosteller
+MGSH
+
+150% of Tuition fee + Hostel fee
+
+
+
+
+...
+Sample Input:
+MSH
+40000
+50000
+Sample Output:
+90000.00
